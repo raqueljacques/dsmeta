2dba1337ebc01b3a5873397f8a3628f0a6512d08
raquel <rvitoria@essia.com>
2022-11-15 19:38:20 -0300

    Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,18 @@
+import SalesCard from "./components/SalesCard"
+import Header from "./components/Header"
+
 function App() {
   return(
-    <h1>Olá mundo!</h1>
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
