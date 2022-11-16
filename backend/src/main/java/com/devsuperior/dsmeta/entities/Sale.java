f138ba4643113be242bbc738ace7e3b0c8d91775
raquel <rvitoria@essia.com>
2022-11-16 16:31:55 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_sales")
 public class Sale {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -22,54 +22,53 @@ public class Sale {
 	private LocalDate date;
 	
 	public Sale() {
-		
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getSellerName() {
 		return sellerName;
 	}
-	
+
 	public void setSellerName(String sellerName) {
 		this.sellerName = sellerName;
 	}
-	
+
 	public Integer getVisited() {
 		return visited;
 	}
+
 	public void setVisited(Integer visited) {
 		this.visited = visited;
 	}
-	
+
 	public Integer getDeals() {
 		return deals;
 	}
+
 	public void setDeals(Integer deals) {
 		this.deals = deals;
 	}
-	
+
 	public Double getAmount() {
 		return amount;
 	}
-	
+
 	public void setAmount(Double amount) {
 		this.amount = amount;
 	}
-	
+
 	public LocalDate getDate() {
 		return date;
 	}
-	
+
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
-
-	
 }
